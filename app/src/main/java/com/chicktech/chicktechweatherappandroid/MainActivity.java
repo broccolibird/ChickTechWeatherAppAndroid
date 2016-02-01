0ba1d331d8e44a4fafacb3de8e75688bbc5bc3f5
Katarina Mitchell <kat@uncorkedstudios.com>
2016-02-01 13:22:58 -0800

    initial commit

---
+++ app/src/main/java/com/chicktech/chicktechweatherappandroid/MainActivity.java
@@ -0,0 +1,25 @@
+package com.chicktech.chicktechweatherappandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
