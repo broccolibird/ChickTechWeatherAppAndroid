128b57640e93081b12bac1f2b6949e3ce0910577
Katarina Mitchell <kat@uncorkedstudios.com>
2016-02-01 13:35:30 -0800

    Requesting weather data

--- app/src/main/java/com/chicktech/chicktechweatherappandroid/MainActivity.java
+++ app/src/main/java/com/chicktech/chicktechweatherappandroid/MainActivity.java
@@ -15,11 +15,38 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private static final String API_KEY = "85709eec804fd0f1065b210fa3dc2548";
+
+    private static final String URL = "https://api.forecast.io/forecast/%s/%f,%f";
+
+    private double latitude = 45.5200;
+
+    private double longitude = -122.6819;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String forecastURL = String.format(URL, API_KEY, latitude, longitude);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(forecastURL).build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "Failed to get weather data", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, "onResponse: " + response.body().string());
+            }
+        });
 
     }
 }
